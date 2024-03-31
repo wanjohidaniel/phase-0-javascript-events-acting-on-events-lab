7f58c061856213f8c75358b77d1394529ec1265f
Daniel Wanjohi <danirunguwanjohi@gmail.com>
2024-03-31 03:48:07 -0700

    Moving Things with JavaScript

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Your code here
+const dodger = document.getElementById('dodger');
+
+function moveDodgerLeft() {
+    const leftPosition = parseInt(dodger.style.left.replace('px', ''), 10);
+    if (leftPosition > 0) {
+        dodger.style.left = `${leftPosition - 1}px`;
+    }
+}
+
+function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    const currentPosition = parseInt(dodger.style.left.replace('px', ''));
+    const newPosition = currentPosition + 10; 
+
+    if (newPosition > 179) {
+        dodger.style.left = `${newPosition}px`;
+    }
+}
